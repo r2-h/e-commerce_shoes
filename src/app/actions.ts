2b29f081b769ae3e44925595f0a594739ee0a581
Artur Khareksian <r2mail1122@gmail.com>
2024-07-07 21:45:25 +0300

    feat: add banner page

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,6 +1,6 @@
 "use server"
 import { parseWithZod } from "@conform-to/zod"
-import { productSchema } from "@/lib/zodSchema"
+import { bannerSchema, productSchema } from "@/lib/zodSchema"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { redirect } from "next/navigation"
 import prisma from "@/lib/db"
@@ -95,4 +95,47 @@ export async function deleteProduct(formData: FormData) {
   })
 
   redirect("/dashboard/products")
+}
+
+export async function createBanner(prevState: any, formData: FormData) {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  if (!user || user.email !== "hareksian23@gmail.com") {
+    return redirect("/")
+  }
+
+  const submission = parseWithZod(formData, {
+    schema: bannerSchema,
+  })
+
+  if (submission.status !== "success") {
+    return submission.reply()
+  }
+
+  await prisma.banner.create({
+    data: {
+      title: submission.value.title,
+      imageString: submission.value.imageString,
+    },
+  })
+
+  redirect("/dashboard/banner")
+}
+
+export async function deleteBanner(formData: FormData) {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  if (!user || user.email !== "hareksian23@gmail.com") {
+    return redirect("/")
+  }
+
+  await prisma.banner.delete({
+    where: {
+      id: formData.get("bannerId") as string,
+    },
+  })
+
+  redirect("/dashboard/banner")
 }
